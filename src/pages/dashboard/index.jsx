387fe55cdd0f4f2d8525728623e85763fec3c4ad
Oprea Catalin <copreapt@gmail.com>
2023-08-16 15:30:54 +0300

    initial commit

---
+++ src/pages/dashboard/index.jsx
@@ -0,0 +1,8 @@
+import Profile from "./Profile";
+import AddJob from './AddJob'
+import Stats from './Stats';
+import AllJobs from './AllJobs'
+import SharedLayout from "./SharedLayout";
+
+
+export { Profile, AddJob, AllJobs, Stats, SharedLayout };
